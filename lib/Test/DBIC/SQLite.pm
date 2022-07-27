04b912b0cb6e2d0aef100f1548ea86d19ffdb97f
Abe Timmerman <abeltje@cpan.org>
2022-07-27 09:38:03 +0200

    Autocommit for distribution Test::DBIC::SQLite 1.01 (minor)

--- lib/Test/DBIC/SQLite.pm
+++ lib/Test/DBIC/SQLite.pm
@@ -2,7 +2,7 @@ package Test::DBIC::SQLite;
 use Moo;
 with 'Test::DBIC::DBDConnector';
 
-our $VERSION = "1.00_02";
+our $VERSION = "1.01";
 
 use parent 'Test::Builder::Module';
 our @EXPORT = qw( connect_dbic_sqlite_ok drop_dbic_sqlite_ok );
